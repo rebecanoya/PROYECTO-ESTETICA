fe187435e6cf8161c537a741018645c2e70bd001
Rebeca Noya <rebecanoyait@gmail.com>
2024-03-11 12:46:10 +0100

    cesta

--- app/public/html/Cesta.php
+++ app/public/html/Cesta.php
@@ -7,6 +7,8 @@
     <link rel="stylesheet" href="../css/general.css">
     <link rel="stylesheet" href="../css/header.css">
     <link rel="stylesheet" href="../css/cesta.css">
+    <script src="https://kit.fontawesome.com/dc2d3ea46f.js" crossorigin="anonymous"></script>
+
 
     <title>Document</title>
 </head>
@@ -26,19 +28,20 @@
                 <h2>Productos</h2>
                 <div class="product">
                     <div class="product-titles">
+                        <span class="title">Foto</span>
                         <span class="title">Producto</span>
                         <span class="title">Precio</span>
                         <span class="title">Cantidad</span>
                         <span class="title">Subtotal</span>
                     </div>
                     <div class="product-info">
+                        <span class="product-img">
+                            <img src="../img/lavanda.png" width="100px" alt="">
+                        </span>
                         <span class="product-name">Producto 1</span>
                         <span class="product-price">$10</span>
                         <span class="product-quantity">1</span>
                         <span class="product-subtotal">$10</span>
-                    </div>
-                    <div class="product-actions">
-                        <button class="remove-btn">Eliminar</button>
                     </div>
                 </div>
                 <!-- More products can be added here -->
@@ -46,10 +49,14 @@
             <div class="summary">
                 <h2>Resumen de Compra</h2>
                 <div class="total">
-                    Total: $45
+                    <p>Subtotal 36,89 €</p>
+                    <p>Total incluyendo impuestos 36,89 €</p>
+
                 </div>
-                <button class="checkout-btn">Comprar</button>
-                <button class="continue-shopping-btn">Seguir Comprando</button>
+                <div class="opciones">
+                    <button class="checkout-btn">Realizar pedido</button>
+                    <a href="./index.php" class="volverCompra">Seguir Comprando</a>
+                </div>
             </div>
         </div>
 
