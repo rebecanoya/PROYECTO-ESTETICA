7e0d4d1b44e079efae2a35d0f441e7fae86f767a
rebeca <rebecanoyait@gmail.com>
2024-03-13 21:22:39 +0100

    productos cesta añadidos

--- app/public/html/Cesta.php
+++ app/public/html/Cesta.php
@@ -3,8 +3,12 @@ include '../../src/iniciarPHP.php';
 $sesion->login('rebeca@teis.com', 'admin');
 // var_dump($_SESSION);
 
-$sql = "SELECT Nombre,Precio from productos where ID in (" . implode(',', array_keys($_SESSION["Carrito"])) . ")";
-var_dump($sql);
+$sql = "SELECT Nombre,Precio,ID from productos where ID in (" . implode(',', array_keys($_SESSION["Carrito"])) . ")";
+// var_dump($sql);
+
+$productos = $BBDD->select($sql);
+
+
 ?>
 
 <!DOCTYPE html>
@@ -35,31 +39,43 @@ var_dump($sql);
         <div class="container">
             <div class="products">
                 <h2>Productos</h2>
-                <div class="product">
-                    <div class="product-titles">
-                        <span class="title">Foto</span>
-                        <span class="title">Producto</span>
-                        <span class="title">Precio</span>
-                        <span class="title">Cantidad</span>
-                        <span class="title">Subtotal</span>
+                <?php
+                $subtotal = 0;
+                foreach ($productos as $producto) {
+
+                    $idProducto = $producto["ID"];
+                    $nombreProducto = $producto["Nombre"];
+                    $precioProducto = $producto["Precio"];
+                    $cantidadProducto = $_SESSION["Carrito"][$idProducto];
+                    $subtotalProducto = $precioProducto * $cantidadProducto;
+                    $subtotal += $subtotalProducto;
+
+                ?>
+                    <div class="product">
+                        <div class="product-titles">
+                            <span class="title">Foto</span>
+                            <span class="title">Producto</span>
+                            <span class="title">Precio</span>
+                            <span class="title">Cantidad</span>
+                            <span class="title">Subtotal</span>
+                        </div>
+                        <div class="product-info">
+                            <span class="product-img">
+                                <img src="../img/productos/<?php echo $idProducto ?>.png" width="100px" alt="">
+                            </span>
+                            <span class="product-name"><?php echo $nombreProducto ?></span>
+                            <span class="product-price"><?php echo $precioProducto ?>€</span>
+                            <span class="product-quantity"><?php echo $cantidadProducto ?></span>
+                            <span class="product-subtotal"><?php echo $subtotalProducto ?>€</span>
+                        </div>
                     </div>
-                    <div class="product-info">
-                        <span class="product-img">
-                            <img src="../img/lavanda.png" width="100px" alt="">
-                        </span>
-                        <span class="product-name">Producto 1</span>
-                        <span class="product-price">$10</span>
-                        <span class="product-quantity">1</span>
-                        <span class="product-subtotal">$10</span>
-                    </div>
-                </div>
-                <!-- More products can be added here -->
+                <?php } ?>
             </div>
             <div class="summary">
                 <h2>Resumen de Compra</h2>
                 <div class="total">
-                    <p>Subtotal 36,89 €</p>
-                    <p>Total incluyendo impuestos 36,89 €</p>
+                    <p>Subtotal <?php echo $subtotal ?> €</p>
+                    <p>Total incluyendo impuestos <?php echo $subtotal ?> €</p>
 
                 </div>
                 <div class="opciones">
