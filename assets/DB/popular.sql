a27443a89ca29193bca20b255271dfb6bb8a2033
rebeca <rebecanoyait@gmail.com>
2024-11-07 21:44:37 +0100

    Update popular.sql

--- assets/DB/popular.sql
+++ assets/DB/popular.sql
@@ -1,9 +1,9 @@
 INSERT INTO `roles`(`rol`) VALUES('admin'),('alumno'),('cliente');
 
-INSERT INTO`lineas` ( `ID_Musica`, `Nombre`, `Color`,`Descripcion`) VALUES 
-( '1', 'Revitalizante', 'FFC0CB',
+INSERT INTO`lineas` ( `Nombre`, `Color`,`Descripcion`) VALUES 
+( 'Revitalizante', 'FFC0CB',
 'Elaborada con precisión e infundida con los ingredientes revitalizantes más potentes de la naturaleza, esta gama de cuidado de la piel está diseñada para dar nueva vida a tu piel, revelando un cutis radiante y rejuvenecido.'),
-('2', 'Relajante', 'AF49FC',
+('Relajante', 'AF49FC',
 'Con precisión y los ingredientes más potentes, esta gama de cuidado de la piel da vida a tu cutis, revelando una piel radiante y rejuvenecida. Déjate envolver por la frescura y el rejuvenecimiento que esta línea ofrece, y experimenta la sensación de una piel revitalizada y llena de vitalidad.');
 INSERT INTO `productos` (`Precio`, `Stock`, `Descripcion`, `ID_Linea`, `Nombre`) VALUES 
 ('20', '10', 'demo', '1', 'Aceite'),
